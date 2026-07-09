9b4e04685c5004b8758d75daa0e7d716ae3b4cd0
Vinicius <ascarialvesvinicius@gmail.com>
2026-07-09 10:12:21 -0300

    Create funcoes.js

---
+++ aula01/funcoes/funcoes.js
@@ -0,0 +1,17 @@
+
+
+// funções
+
+function mostrarNome() {
+    console.log("ola seja bem vindo ao sistema!")
+
+    console.log("Aprendendo Algoritimos!")
+}
+
+function gerarNumeroAleatorio() {
+    return Math.random();
+}
+
+let numeroGerado = gerarNumeroAleatorio();
+
+console.log("Numero Gerado foi " + numeroGerado);
