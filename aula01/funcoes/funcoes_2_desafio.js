edcf76e9860ab307a9e953b8a39d20bd718a182f
Vinicius <ascarialvesvinicius@gmail.com>
2026-07-09 10:52:34 -0300

    Update funcoes_2_desafio.js

--- aula01/funcoes/funcoes_2_desafio.js
+++ aula01/funcoes/funcoes_2_desafio.js
@@ -10,17 +10,18 @@ function mostrarNome(nome) {
 const nota1 = 8;
 const nota2 = 10;
 const nota3 = 7;
+const nome = "Vinicius";
 
-function calculoMedia(nota1, nota2, nota3) {
+function calculoMedia(nota1, nota2, nota3, nome) {
     let media = (nota1 + nota2 + nota3) / 3;
-    return media;
+    return `O aluno(a) ${nome} teve a media de ${media}`;
 }
 
-const mediaVinicius = calculoMedia(nota1, nota2, nota3)
+const mediaVinicius = calculoMedia(nota1, nota2, nota3, nome)
 
 console.log(mediaVinicius);
 
 
-const mediaFulano = calculoMedia(8, 10, 6);
+const mediaFulano = calculoMedia(8, 10, 6, "Victoria");
 
 console.log(mediaFulano)
