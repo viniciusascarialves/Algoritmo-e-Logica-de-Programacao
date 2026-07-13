3bb86b7b15c5e4f5fe6dc7ab147139ae04401646
Vinicius <ascarialvesvinicius@gmail.com>
2026-07-13 09:47:38 -0300

    Update funcoes_2_desafio.js

--- aula01/funcoes/funcoes_2_desafio.js
+++ aula01/funcoes/funcoes_2_desafio.js
@@ -25,3 +25,25 @@ console.log(mediaVinicius);
 const mediaFulano = calculoMedia(8, 10, 6, "Victoria");
 
 console.log(mediaFulano)
+
+
+// ============================================
+
+/*
+const valorCapital = 2000;
+const taxaJuros = 0.03;
+const totalPeriodo = 12;
+
+const JurosFinal = valorCapital * taxaJuros * totalPeriodo;
+
+console.log(`Juros no periodo de ${totalPeriodo} meses é de ${JurosFinal} reais`);
+*/
+
+function CalculadoraJuros(valorCapital, toralPeriodo) {
+    const juros = 0.3; // 30%
+
+    const JurosFinal = valorCapital * juros * toralPeriodo;
+    console.log(`Juros no periudo de ${toralPeriodo} meses é de R$ ${JurosFinal} reais`)
+}
+
+CalculadoraJuros(2000, 12);
