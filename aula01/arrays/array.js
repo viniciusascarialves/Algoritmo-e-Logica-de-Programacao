41b819c433ada6bb73dd398cd5091dff52601711
Vinicius <ascarialvesvinicius@gmail.com>
2026-07-20 07:48:05 -0300

    Update array.js

--- aula01/arrays/array.js
+++ aula01/arrays/array.js
@@ -1,4 +1,12 @@
 
 // Arrays - Sao colecoes de dados.
 
-const numeros = 1
+const numeros = [1, 2, 10, 300, "Vinicius"]
+
+
+// INDEX            0           1         2       3
+const nomes = ["Vinicius", "Matheus", "Lucas", "Ana"]
+
+console.log(nomes[1]);
+
+console.log(nomes.length)
