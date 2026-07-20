28c89804ea41835f7eb331996fb7a2d235411766
Vinicius <ascarialvesvinicius@gmail.com>
2026-07-20 07:59:33 -0300

    Create manipular_array.js

---
+++ aula01/arrays/manipular_array.js
@@ -0,0 +1,18 @@
+const numeros = [1, 5, 10, 20];
+
+/*
+  push > Adiciona um item ou mais no ultimo elemento do array
+  pop > Remove o ultimo elemento do array
+  unshift > Adicionar elementos no inicio do array
+  shift > Remover o primeiro elemento do array
+*/
+
+numeros.push(30, 50, 100);
+
+// numeros.pop();
+
+// numeros.unshift(500);
+
+numeros.shift();
+
+console.log(numeros);
