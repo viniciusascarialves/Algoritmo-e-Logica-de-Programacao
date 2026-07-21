6dda477bf1a3619befa323b848eba5b1dc683c38
Vinicius <ascarialvesvinicius@gmail.com>
2026-07-21 09:17:09 -0300

    Create array_indexof.js

---
+++ aula01/arrays/array_indexof.js
@@ -0,0 +1,16 @@
+
+
+// Join
+
+const nomes = ["Matheus", "Lucas", "Ana", "Fulano"];
+//console.log(nomes);
+
+
+const listaNomes = nomes.join(", ")
+
+//console.log(listaNomes);
+
+
+// indexOf
+const posicaoItem = nomes.indexOf("fulano")
+console.log(posicaoItem);
