bec9e7d75a79fe5dbffed23a85bc43220c15654c
Vinicius <ascarialvesvinicius@gmail.com>
2026-07-21 09:00:00 -0300

    Update array_splice.js

--- aula01/arrays/array_splice.js
+++ aula01/arrays/array_splice.js
@@ -5,3 +5,10 @@
    Quantidade: O numero de elementos a serem removidos a partir daquela posição.
    elementos que deseja adicionar naquele index (opcional)
 */
+
+//               0  1   2   3
+const numeros = [1, 5, 20, 10];
+
+numeros.slice(1, 1, 30);
+
+console.log(numeros);
