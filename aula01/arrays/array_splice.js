df9734a0aa178e469654d8f825a2c8e46d1045ef
Vinicius <ascarialvesvinicius@gmail.com>
2026-07-21 08:58:31 -0300

    Update array_splice.js

---
+++ aula01/arrays/array_splice.js
@@ -0,0 +1,7 @@
+
+// Splice:
+/*
+   Index (indice): A posição do array onde a operacao começará.
+   Quantidade: O numero de elementos a serem removidos a partir daquela posição.
+   elementos que deseja adicionar naquele index (opcional)
+*/
