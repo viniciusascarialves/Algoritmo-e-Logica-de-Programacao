46ad08bab9c55cdeba68668b9033c2f3d24f1c9b
Vinicius <ascarialvesvinicius@gmail.com>
2026-07-27 09:34:35 -0300

    Update array_foreach.js

--- aula01/arrays/array_foreach.js
+++ aula01/arrays/array_foreach.js
@@ -3,3 +3,20 @@
 // Includes verifica se existe o item no array e devolve verdadeiro ou falso
 
 const nomes = ["Vinicius", "Lucas", "Ana", "Fulano"];
+const verificaNome = nomes.includes("Fulano")
+
+// foreach
+const alunos = ["Matheus", "Vinicius", "Ana", "Maria"];
+
+
+
+alunos.forEach((nome) => {
+    console.log(`Bem vindo(a) ${nome}`)
+})
+
+
+const valores = [10, 500, 300];
+
+valores.forEach((valor) => {
+    const conta = valor + 150;
+})
