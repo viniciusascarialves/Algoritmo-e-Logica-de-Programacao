3a3337abb48c00d1c008de5048d94390b89d1c7b
Vinicius <ascarialvesvinicius@gmail.com>
2026-07-28 09:32:37 -0300

    Update desafio1.js

--- aula01/arrays/desafio1.js
+++ aula01/arrays/desafio1.js
@@ -4,3 +4,11 @@
 
 -> Você precisa remover o cargo "backend" da lista de cargos pois ese cargo não esta mais disponivel na empresa
 */
+
+
+// 1 Montar a lista
+// 2 encontrar onde esta o "backend"
+// 3 remover este cargo da lista
+
+
+const cargos = ["fullstack", "frontend", "backend", "mobile"];
