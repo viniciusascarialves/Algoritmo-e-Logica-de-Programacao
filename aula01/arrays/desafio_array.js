646bb03e4a410b2c60f6ee9a7df2e5cc8ee2f9e3
Vinicius <ascarialvesvinicius@gmail.com>
2026-07-28 09:22:33 -0300

    Update desafio_array.js

--- aula01/arrays/desafio_array.js
+++ aula01/arrays/desafio_array.js
@@ -2,5 +2,5 @@
 ## Desafio 1 -
 -> Crie uma array com alguns cargos de uma empresa (por exemplo, "fullstack", "frontend", "backend", "mobile")
 
--> Voce precisa remover o cargo "backend" da lista de cargos pois ese cargo não esta mais disponivel na empresa
+-> Você precisa remover o cargo "backend" da lista de cargos pois ese cargo não esta mais disponivel na empresa
 */
