073c0fe85d84f822e855439d9863047d2c7a194e
Vinicius <ascarialvesvinicius@gmail.com>
2026-07-29 09:47:45 -0300

    Update desafio_array.js

--- aula01/arrays/desafio_array.js
+++ aula01/arrays/desafio_array.js
@@ -11,6 +11,6 @@
 -> Crie um array com os nomes abaixo:
 ["Alice", "Lucas", "Carol", "David"];
 
--> Voce esta procurando por um nome chamado henrique caso o nome esteja na lista mostre no colsole qual a posicao em que ele esse nome esta na sua lista.
+-> você está procurando por um nome chamado Henrique caso o nome esteja na lista mostre no colsole qual a posicao em que ele esse nome esta na sua lista.
 -> Caso nao encontre esase nome mostre no console o nome procurado e efale que nao foi encontrada na lista
 */
