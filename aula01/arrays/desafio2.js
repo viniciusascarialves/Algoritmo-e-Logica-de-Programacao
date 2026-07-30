0aa173750171c6214010d94908fe674afc9ad182
Vinicius <ascarialvesvinicius@gmail.com>
2026-07-30 08:18:02 -0300

    Update desafio2.js

--- aula01/arrays/desafio2.js
+++ aula01/arrays/desafio2.js
@@ -6,3 +6,6 @@
 -> você está procurando por um nome chamado Henrique caso o nome esteja na lista mostre no colsole qual a posicao em que ele esse nome esta na sua lista.
 -> Caso nao encontre esase nome mostre no console o nome procurado e efale que nao foi encontrada na lista
 */
+
+
+const nomes = ["Alice", "Lucas", "Carol", "David"];
