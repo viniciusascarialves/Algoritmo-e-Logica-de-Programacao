ba1b0d612a7cc58870c07be4812eeb16b28ac950
Vinicius <ascarialvesvinicius@gmail.com>
2026-07-31 10:15:48 -0300

    Update desafio3.js

--- aula01/arrays/desafio3.js
+++ aula01/arrays/desafio3.js
@@ -1,6 +1,15 @@
 /*
 ## Desafio 3 -
-Crie um array com esses numeros: [5, 15, 23, 2]
+Crie um array com esses numeros: [5, 15, 23, 2];
 
 - Voce deve devolver qual e a soma de todos esses numeros usando foreach
 */
+
+const numeros = [5, 15, 23, 2, ];
+let somaTotal = 0;
+
+numeros.forEach((numero) => {
+    somaTotal = somaTotal + numero
+})
+
+console.log(`A soma total da lista e ${somaTotal}`)
