22f9131ed0e9ceb1d885d7bae52e15b6a6eb5853
Vinicius <ascarialvesvinicius@gmail.com>
2026-08-05 10:24:35 -0300

    Update desafio_array.js

--- aula01/arrays/desafio_array.js
+++ aula01/arrays/desafio_array.js
@@ -37,5 +37,8 @@ Crie um array com esses numeros: [6, 12, 5, 9, 14, 27];
 
 /*
 ## Desafio 5 -
+Filtrar palavras com mais de 5 letras utilizando o foreach
+const = palavras = ["gato", "cachorro", "elefante", "peixe", "leao"];
 
+- Voce deve desenvolver um array com todas as palavras que possuem mais de 5 letras
 */
