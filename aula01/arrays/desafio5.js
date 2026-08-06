f5237dd13a32dfcbb1f6868c5c66daea7d2bc372
Vinicius <ascarialvesvinicius@gmail.com>
2026-08-06 09:27:07 -0300

    Create desafio5.js

---
+++ aula01/arrays/desafio5.js
@@ -0,0 +1,7 @@
+/*
+## Desafio 5 -
+Filtrar palavras com mais de 5 letras utilizando o foreach
+const = palavras = ["gato", "cachorro", "elefante", "peixe", "leao"];
+
+- Voce deve desenvolver um array com todas as palavras que possuem mais de 5 letras
+*/
