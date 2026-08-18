2752e7327fe5f6d1327460e1807111f99cd0078c
Vinicius <ascarialvesvinicius@gmail.com>
2026-08-18 07:17:39 -0300

    Create map.js

---
+++ aula01/arrays/map.js
@@ -0,0 +1,19 @@
+
+// Map
+
+const alunos = ["Matheus", "Lucas", "Ana", "Maria"]
+
+const resultado = alunos.map((aluno) => {
+    return aluno + " Online"
+})
+
+
+const numeros = [1, 5, 10];
+
+const ValoresDobrados = numeros.map((numero) => {
+    return numero * 2
+})
+
+console.log(ValoresDobrados);
+
+console.log(numeros);
