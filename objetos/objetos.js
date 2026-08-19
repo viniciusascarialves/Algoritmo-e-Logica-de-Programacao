aa921b795385184f22e82b259885fc0ec52dec5c
Vinicius <ascarialvesvinicius@gmail.com>
2026-08-19 05:29:25 -0300

    Update objetos.js

--- objetos/objetos.js
+++ objetos/objetos.js
@@ -1,2 +1,18 @@
 
 // Objetos
+const aluno = {
+    nome: "Vinicius",
+    idade: 28,
+    cargo: "Full Stack"
+}
+
+console.log(aluno);
+console.log(aluno.nome)
+console.log(alunp.idade)
+
+
+aluno.cargo = "Frontend"
+aluno.status = true
+
+console.log(aluno["cargo"])
+console.log(aluno.status);
