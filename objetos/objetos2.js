30f4f035bc1388463b48e04dcc88eaca32144d68
Vinicius <ascarialvesvinicius@gmail.com>
2026-08-19 05:41:40 -0300

    Create objetos2.js

---
+++ objetos/objetos2.js
@@ -0,0 +1,13 @@
+
+const pessoa = {
+    nome: "Vinicius",
+    idade: 18,
+    cargo: "Frontend",
+    saudacao: function () {
+        console.log("Ola eu sou o Vinicius!")
+    }
+}
+
+
+//console.log(pessoa);
+//console.log(saudacao);
