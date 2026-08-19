8b720c30c2893a6b5049ef9968045aa6f461192b
Vinicius <ascarialvesvinicius@gmail.com>
2026-08-19 06:17:51 -0300

    Update objetos3.js

--- objetos/objetos3.js
+++ objetos/objetos3.js
@@ -4,11 +4,13 @@
 const pessoa = {
     nome: "Vinicius",
     idade: 18,
-    cargo: "Frontend"
+    cargo: "Frontend",
     endereco: {
-        rua: "Avenida dev 123"
-        complemento: "Em frente a praca"
-        cep: 79000120
-        numero: 10
+        rua: "Avenida dev 123",
+        complemento: "Em frente a praca",
+        cep: 79000120,
+        numero: 10,
     }
 }
+
+console.log(pessoa)
