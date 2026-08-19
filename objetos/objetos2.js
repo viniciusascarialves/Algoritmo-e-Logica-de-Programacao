3303b57fd8c956f3e4ff3492dde0ebe4bbd9a33b
Vinicius <ascarialvesvinicius@gmail.com>
2026-08-19 05:45:25 -0300

    Update objetos2.js

--- objetos/objetos2.js
+++ objetos/objetos2.js
@@ -11,3 +11,19 @@ const pessoa = {
 
 //console.log(pessoa);
 //console.log(saudacao);
+
+const calculos = {
+    soma: function (numero1, numero2) {
+        return numero1 + numero2
+    },
+    subtrair: function (numero1, numero2) {
+        return numero1 - numero2;
+    }
+}
+
+
+const resultadoSoma = calculos.soma(10, 20)
+console.log(resultadoSoma);
+
+const resultadoSubtracao = calculos.subtrair(30, 12)
+console.log(resultadoSubtracao)
