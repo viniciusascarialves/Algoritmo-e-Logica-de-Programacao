4c7e51aedee6b27c841d4f7d7875a935451ca269
Vinicius <ascarialvesvinicius@gmail.com>
2026-08-19 06:16:47 -0300

    Create objetos3.js

---
+++ objetos/objetos3.js
@@ -0,0 +1,14 @@
+
+// Objetos com propriedades aninhadas
+
+const pessoa = {
+    nome: "Vinicius",
+    idade: 18,
+    cargo: "Frontend"
+    endereco: {
+        rua: "Avenida dev 123"
+        complemento: "Em frente a praca"
+        cep: 79000120
+        numero: 10
+    }
+}
