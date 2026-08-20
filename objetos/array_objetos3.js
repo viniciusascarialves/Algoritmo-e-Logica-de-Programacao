20a3fb4d594115e0d948099c46750a685666eb19
Vinicius <ascarialvesvinicius@gmail.com>
2026-08-20 07:29:53 -0300

    Update array_objetos3.js

---
+++ objetos/array_objetos3.js
@@ -0,0 +1,10 @@
+/*
+ > Crie uma lista de contatos onde deve ter o nome da pessoa e o telefone
+ > Como encontrar um nome dentro da lista de contatos pesquisando pelo nome?
+ */
+
+ const contatos = [
+    { nome: "Matheus", telefone: "99910000" },
+    { nome: "Vinicius", telefone: "8210000" },
+    { nome: "Jose", telefone: "919110000" },
+ ]
