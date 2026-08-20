b5812e4dc1c46d235ff835a4474f3e985e215e20
Vinicius <ascarialvesvinicius@gmail.com>
2026-08-20 07:50:17 -0300

    Create pilha.js

---
+++ aula01/desafio/pilha.js
@@ -0,0 +1,8 @@
+/*
+# Crie um algoritmo de pilha.
+- Deverá ter a função para retornar o tamanho da pilha.
+- Função para adicionar um elemento na pilha.
+- Função para retornar qual é o último elemento da pilha.
+- Função para remover o último elemento da pilha e retonar qual foi o elemento removido.
+- Função para limpar a pilha e zerar ela.
+*/
