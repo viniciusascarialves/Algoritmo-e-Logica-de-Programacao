ee0cdd12a264751ced451656d52576698b3942d9
Vinicius <ascarialvesvinicius@gmail.com>
2026-08-20 06:40:39 -0300

    Update array_objetos.js

--- objetos/array_objetos.js
+++ objetos/array_objetos.js
@@ -4,17 +4,17 @@ const alunos = [
     {
         nome: "Vinicius",
         idade: 20,
-        cargo: "Frontend"
+        cargo: "Frontend",
     },
     {
         nome: "Ana",
         idade: 30,
-        cargo: "FullStack"
+        cargo: "FullStack",
     },
     {
-        nome: "Lucas"
+        nome: "Lucas",
         idade: 23,
-        cargo: "RH"
+        cargo: "RH",
     }
 ]
 
@@ -24,4 +24,6 @@ console.log(alunos[0].cargo)
 alunos.forEach((aluno) => {
     console.log(`Aluno(a) ${aluno.nome}`);
     console.log(`Cargo Atual ${aluno.cargo}`);
+
+    console.log("==================")
 })
