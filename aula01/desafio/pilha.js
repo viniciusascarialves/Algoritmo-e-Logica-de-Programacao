50d39549b12d1e1dc376e959b57dc51066335a13
Vinicius <ascarialvesvinicius@gmail.com>
2026-08-21 07:45:09 -0300

    Update pilha.js

--- aula01/desafio/pilha.js
+++ aula01/desafio/pilha.js
@@ -6,3 +6,27 @@
 - Função para remover o último elemento da pilha e retonar qual foi o elemento removido.
 - Função para limpar a pilha e zerar ela.
 */
+
+function criarPilha() {
+    let elementos = [];
+
+    function adicionar(elemento) {
+        elementos.push(elemento)
+    }
+
+    function tamanho() {
+        return elementos.length;
+    }
+
+    return {
+        adicionar
+        tamanho
+    }
+}
+
+let pilha = criarPilha();
+
+pilha.adicionar(10);
+pilha.adicionar(20);
+
+console.log(`O tamanho da pilha e ${pilha.tamanho()}`)
